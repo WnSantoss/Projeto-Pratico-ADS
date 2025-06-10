0eee1493c2ce035ada631437adc4ef378426f39a
WnSantoss <wellington.santo0s51@gmail.com>
2025-06-09 21:09:01 -0300

    app.py

--- app.py
+++ app.py
@@ -182,4 +182,4 @@ def remover_usuario(usuario_id):
 if __name__ == '__main__':
     with app.app_context():
         inicializar_banco()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
